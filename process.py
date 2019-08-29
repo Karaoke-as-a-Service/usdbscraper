5daea12b17640502e854130cf538d68bbc8b6fe0
luto <m@luto.at>
2019-08-29 19:13:30 +0200

    improve error handling, debug output

--- process.py
+++ process.py
@@ -5,6 +5,7 @@ import hashlib
 import bs4
 import os
 import re
+import sys
 
 
 
@@ -55,23 +56,27 @@ def main():
 
     for id in open('animux_ids.txt').readlines():
         content = get(s, url_editsongs(id))
-        soup = bs4.BeautifulSoup(content, features='lxml')
-        content = soup.select('textarea')[0].get_text()
-        filename = soup.select('input[name="filename"]')[0]['value']
-        print(filename)
+        print(f'processing id {id.strip()}')
+        try:
+            soup = bs4.BeautifulSoup(content, features='lxml')
+            content = soup.select('textarea')[0].get_text()
+            filename = soup.select('input[name="filename"]')[0]['value']
+            print(f'found filename {filename}, {len(content)} bytes')
 
-        filename = filename.replace('/', '_')
+            filename = filename.replace('/', '_')
 
-        write_text(f'files/{filename}', content)
+            write_text(f'files/{filename}', content)
 
-        content = get(s, url_detail(id))
-        if not isinstance(content, str):
-            content = content.decode()
-        youtube_links = youtube_link.findall(content)
+            content = get(s, url_detail(id))
+            if not isinstance(content, str):
+                content = content.decode()
+            youtube_links = youtube_link.findall(content)
 
-        if youtube_links:
-            write_text(f'files/{filename}_youtube', '\n'.join(set(youtube_links)) + '\n')
-
+            if youtube_links:
+                write_text(f'files/{filename}_youtube', '\n'.join(set(youtube_links)) + '\n')
+        except Exception as ex:
+            lineno = sys.exc_info()[-1].tb_lineno
+            print(f'failed: {str(ex)}, line {lineno}', file=sys.stderr)
 
 if __name__ == '__main__':
     main()
